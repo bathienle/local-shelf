cf70b2084cc5e803eb0042de4ddbb3f2b1fcdd27
Ba Thien Le <bathien.le.pro@gmail.com>
2025-07-26 20:06:32 +0200

    Fix ingredient list display (#8)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,27 +1,12 @@
 <template>
   <div class="container mx-auto px-4 max-w-screen-lg">
     <div class="w-full text-center text-sm text-gray-500 py-4 mt-10">
-      <h1>The ingredients</h1>
+      <h1 class="text-3xl font-bold pb-6">The ingredients</h1>
 
       <div class="grid grid-cols-2 gap-2">
         <ImageUploadForm @submit="handleImageUpload" />
 
         <div class="space-y-4">
-          <div v-for="(ingredient, index) in ingredients" :key="ingredient" class="flex items-center gap-2">
-            <div class="flex-grow flex justify-center gap-1">
-              <IngredientInput
-                :model-value="ingredient"
-                @update:ingredient="(newIngredient) => updateIngredient(index, newIngredient)"
-              />
-              <button
-                class="text-red-500 hover:text-red-700"
-                @click="removeIngredient(index)"
-              >
-                <Icon name="uil:trash" class="text-xl" />
-              </button>
-            </div>
-          </div>
-
           <div class="space-x-2">
             <button
               class="block inline-flex items-center gap-2 mt-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
@@ -38,6 +23,25 @@
               Clear all ingredients
             </button>
           </div>
+
+          <div class="max-h-80 overflow-y-auto space-y-2">
+            <div v-for="(ingredient, index) in ingredients" :key="ingredient.name" class="flex items-center gap-2">
+              <div class="flex-grow flex justify-center gap-1">
+                <IngredientInput
+                  :model-value="ingredient"
+                  @update:ingredient="(newIngredient) => updateIngredient(index, newIngredient)"
+                />
+                <button
+                  class="text-red-500 hover:text-red-700"
+                  @click="removeIngredient(index)"
+                >
+                  <Icon name="uil:trash" class="text-xl" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </div>
     </div>
