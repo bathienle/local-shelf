1810845012d6c95a0d4b2e08be71fa253fe15845
Ba Thien Le <bathien.le.pro@gmail.com>
2025-07-27 21:45:56 +0200

    Add no recipes display

--- app/pages/recipes.vue
+++ app/pages/recipes.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="min-h-screen max-w-5xl mx-auto px-6 py-12">
     <h1 class="text-4xl font-extrabold mb-8 text-blue-700 text-center">Recipes</h1>
+    <div v-if="store.recipes.length === 0" class="text-center">No recipes available.</div>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
       <article
         v-for="recipe in store.recipes"
